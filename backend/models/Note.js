8e3c1129a6e7c2908c0459a1027dac774227a625
Chandan <chandangajendra3@gmail.com>
2022-09-06 22:27:26 +0530

    video no 54 completed

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,7 +1,11 @@
-import mongoose, { model } from 'mongoose';
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
     title: {
         type: String,
         required: true,
@@ -23,5 +27,5 @@ const NotesSchema = new Schema({
 );
 
 const Notes = mongoose.model('Notes', NotesSchema);
-model.exports = Notes;
+module.exports = Notes;
 
